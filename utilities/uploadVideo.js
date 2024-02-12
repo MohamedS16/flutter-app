4f6f476d5f354058ca2dc6433e720c4f3bcb7130
MohamedS16 <115186109+MohamedS16@users.noreply.github.com>
2024-02-12 09:54:41 +0200

    Update uploadVideo.js

--- utilities/uploadVideo.js
+++ utilities/uploadVideo.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 const uploadfunction = () => {
     const diskstorage = multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, '/uploads/videos')
+            cb(null, 'project/src/uploads/videos')
 
         },
         filename: async (req, file, cb) => {
